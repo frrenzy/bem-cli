6d518b9eb6d2f9c29f19c46bf81b95cdb2627a31
Ivan Sizov <thecutecinnamonbun@gmail.com>
2022-06-07 21:06:16 +0300

    install script

--- install.sh
+++ install.sh
@@ -2,13 +2,34 @@
 
 set -e
 
-URL=$(curl -s https://api.github.com/repos/frrenzy/bem-cli/releases/latest \
-        | grep browser_download_url \
-        | cut -d : -f 2,3 \
-        | tr -d \" \
-        | tr -d "[:space:]")
+URLS=("${(@f)$(curl -s https://api.github.com/repos/frrenzy/bem-cli/releases/latest \
+                                 | grep browser_download_url \
+                                 | cut -d : -f 2,3 \
+                                 | tr -d \" \
+                                 | awk '{print substr($0, 2)}')}")
+
+ABOBA="${URLS[@]:0:1}"
+MASK="${ABOBA%_*}"
+
+OS=$(uname -a | awk '{print $1}')
+ARCH=$(uname -a | awk '{print $NF}')
+
+if [[ $OS == "Darwin" ]]
+then
+  if [[ $ARCH == "arm64" ]]
+  then
+    URL="${MASK}_m1"
+    NAME="bem_m1"
+  else
+    URL="${MASK}_intel"
+    NAME="bem_intel"
+  fi
+else
+  URL="${MASK}_deb"
+  NAME="bem_deb"
+fi
 
 wget $URL
 
-chmod +x bem
-mv bem /usr/local/bin/bem
+chmod +x $NAME
+mv $NAME /usr/local/bin/bem

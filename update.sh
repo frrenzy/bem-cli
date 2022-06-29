42d7635b044ffc481f28877bc5d158bedc953f6b
Ivan Sizov <vsizov63@gmail.com>
2022-06-29 22:09:13 +0300

    v2.2.1

--- update.sh
+++ update.sh
@@ -5,45 +5,46 @@ VERSION=$(curl -s https://api.github.com/repos/frrenzy/bem-cli/releases/latest \
                                          | cut -d : -f 2,3 \
                                          | tr -d \" \
                                          | tr -d \, \
+                                         | tr -d v \
                                          | awk '{print substr($0, 2)}')
 
-URLS=("${(@f)$(curl -s https://api.github.com/repos/frrenzy/bem-cli/releases/latest \
-                                 | grep browser_download_url \
-                                 | cut -d : -f 2,3 \
-                                 | tr -d \" \
-                                 | awk '{print substr($0, 2)}')}")
+#URLS=("${(@f)$(curl -s https://api.github.com/repos/frrenzy/bem-cli/releases/latest \
+#                                 | grep browser_download_url \
+#                                 | cut -d : -f 2,3 \
+#                                 | tr -d \" \
+#                                 | awk '{print substr($0, 2)}')}")
+#
+#ABOBA="${URLS[@]:0:1}"
+#MASK="${ABOBA%_*}"
+#
+#OS=$(uname -a | awk '{print $1}')
+#ARCH=$(uname -a | awk '{print $NF}')
+#
+#if [[ $OS == "Darwin" ]]
+#then
+#  if [[ $ARCH == "arm64" ]]
+#  then
+#    URL="${MASK}_m1"
+#    NAME="bem_m1"
+#  else
+#    URL="${MASK}_intel"
+#    NAME="bem_intel"
+#  fi
+#else
+#  URL="${MASK}_deb"
+#  NAME="bem_deb"
+#fi
+#
+#wget "$URL"
+##
+##chmod a+rx $NAME
+##
+##if [ -d "$HOME/bin" ]; then
+##  rm -rf $HOME/bin/bem
+##else
+##  mkdir "$HOME/bin"
+##fi
+#
+#mv $NAME "$HOME/bin/bem"
 
-ABOBA="${URLS[@]:0:1}"
-MASK="${ABOBA%_*}"
-
-OS=$(uname -a | awk '{print $1}')
-ARCH=$(uname -a | awk '{print $NF}')
-
-if [[ $OS == "Darwin" ]]
-then
-  if [[ $ARCH == "arm64" ]]
-  then
-    URL="${MASK}_m1"
-    NAME="bem_m1"
-  else
-    URL="${MASK}_intel"
-    NAME="bem_intel"
-  fi
-else
-  URL="${MASK}_deb"
-  NAME="bem_deb"
-fi
-
-wget "$URL"
-
-chmod a+rx $NAME
-
-if [ -d "$HOME/bin" ]; then
-  rm -rf $HOME/bin/bem
-else
-  mkdir "$HOME/bin"
-fi
-
-mv $NAME "$HOME/bin/bem"
-
-echo "$VERSION"
+echo $VERSION

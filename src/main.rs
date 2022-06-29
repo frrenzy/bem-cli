02ee9fd5ed4b9eb2c9076006de190802f747cf20
Ivan Sizov <vsizov63@gmail.com>
2022-06-29 20:06:24 +0300

    v2.1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::env;
 use std::io::prelude::*;
 use std::path::Path;
 use std::{fs, fs::File};
@@ -6,21 +7,106 @@ use std::process::Command;
 
 fn main() -> io::Result<()> {
     let big_dir = ".";
-
+    let args: Vec<String> = env::args().collect();
     let path = Path::new(big_dir).canonicalize().unwrap();
 
-    println!("{:?}", &path);
+    match args.len() {
+        1 => {
+            eprintln!("Error: missing command");
+            help();
+        },
+        2 => {
+            match &args[1][..] {
+                "update" => update()?,
+                "create" => {
+                    create_bem(&path)?;
+                    loop {
+                        println!("Do you want to install dependencies? [yn]");
 
-    generate_dotfiles(&path)?;
+                        let mut answer = String::new();
 
-    generate_directories(&path)?;
+                        io::stdin()
+                            .read_line(&mut answer)
+                            .expect("Failed to read answer");
 
-    generate_boilerplate(&path)?;
+                        match &answer.trim()[..] {
+                            "y" => {
+                                install()?;
+                                break
+                            },
+                            "n" => break,
+                            _ => println!("Answer should be y or n"),
+                        }
+                    }
+                },
+                "version" => println!("{}", env!("CARGO_PKG_VERSION")),
+                _ => {
+                    eprintln!("Error: invalid command");
+                    help();
+                },
+            }
+        },
+        3 => {
+            match &args[1][..] {
+                "create" => {
+                    match &args[2][..] {
+                        "-i" => create_bem_install(&path)?,
+                        "--install" => create_bem_install(&path)?,
+                        _ => {
+                            eprintln!("Error: invalid command");
+                            help();
+                        },
+                    }
+                },
+                _ => {
+                    eprintln!("Error: invalid command");
+                    help();
+                },
+            }
+        },
+        _ => {
+            eprintln!("Error: invalid command");
+            help();
+        },
+    }
 
+
+    Ok(())
+}
+
+fn update() -> io::Result<()> {
+    Command::new("zsh")
+        .arg("-c")
+        .arg("$(curl -fsSL https://raw.github.com/frrenzy/bem-cli/master/update.sh)")
+        .spawn()
+        .expect("Unable to update :(. Reinstall via install command: https://github.com/frrenzy/bem-cli");
+
+    Ok(())
+}
+
+fn create_bem_install(dir: &Path) -> io::Result<()> {
+    create_bem(&dir)?;
+
+    install()?;
+
+    Ok(())
+}
+
+fn install() -> io::Result<()> {
     Command::new("npm")
         .arg("install")
-        .output()
-        .expect("Failed to execute process");
+        .spawn()
+        .expect("Failed to install dependencies");
+
+    Ok(())
+}
+
+fn create_bem(dir: &Path) -> io::Result<()> {
+    generate_dotfiles(&dir)?;
+
+    generate_directories(&dir)?;
+
+    generate_boilerplate(&dir)?;
 
     Ok(())
 }
@@ -88,4 +174,16 @@ fn generate_boilerplate(dir: &Path) -> io::Result<()> {
     file.write_all(bytes)?;
 
     Ok(())
+}
+
+fn help() {
+    println!("usage:
+bem version
+    Shows version.
+bem update
+    Downloads new version.
+bem create <option>
+    Generates BEM project in current empty folder.
+    Optional argument:
+        -i, --install: install dependencies automatically");
 }
---
+++ update.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+
+set -e
+
+URLS=("${(@f)$(curl -s https://api.github.com/repos/frrenzy/bem-cli/releases/latest \
+                                 | grep browser_download_url \
+                                 | cut -d : -f 2,3 \
+                                 | tr -d \" \
+                                 | awk '{print substr($0, 2)}')}")
+
+ABOBA="${URLS[@]:0:1}"
+MASK="${ABOBA%_*}"
+
+OS=$(uname -a | awk '{print $1}')
+ARCH=$(uname -a | awk '{print $NF}')
+
+if [[ $OS == "Darwin" ]]
+then
+  if [[ $ARCH == "arm64" ]]
+  then
+    URL="${MASK}_m1"
+    NAME="bem_m1"
+  else
+    URL="${MASK}_intel"
+    NAME="bem_intel"
+  fi
+else
+  URL="${MASK}_deb"
+  NAME="bem_deb"
+fi
+
+wget "$URL"
+
+chmod a+rx $NAME
+
+if [ -d "$HOME/bin" ]; then
+  rm -rf $HOME/bin/bem
+else
+  mkdir "$HOME/bin"
+fi
+
+mv $NAME $HOME/bin/bem
